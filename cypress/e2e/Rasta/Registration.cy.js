f953b342be3565ea00b59e1023d4afafe0c08bea
Abdurashid77 <abdurj77777@gmail.com>
2022-11-30 17:46:35 +0500

    Registration added

---
+++ cypress/e2e/Rasta/Registration.cy.js
@@ -0,0 +1,14 @@
+let a = Math.floor(100000 + Math.random() * 900000)
+describe('example to-do app', () => {
+    beforeEach(() => {
+      cy.visit('https://rasta.uz/')
+    })
+    it('Login', () => { 
+        cy.wait(200)
+        cy.get('.Header_buttons__QxOme > .Button_button__bShZq').click()
+        
+
+
+    })
+})
+
