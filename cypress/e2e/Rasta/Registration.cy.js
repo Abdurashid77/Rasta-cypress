a0a2df79750dea2171e3b3ba01c49de6357a9129
Abdurashid77 <abdurj77777@gmail.com>
2022-12-01 10:11:14 +0500

    Full login added

--- cypress/e2e/Rasta/Registration.cy.js
+++ cypress/e2e/Rasta/Registration.cy.js
@@ -1,4 +1,4 @@
-let a = Math.floor(100000 + Math.random() * 900000)
+let a = Math.floor(100000 + Math.random() * 900000000)
 describe('example to-do app', () => {
     beforeEach(() => {
       cy.visit('https://rasta.uz/')
@@ -6,6 +6,14 @@ describe('example to-do app', () => {
     it('Login', () => { 
         cy.wait(200)
         cy.get('.Header_buttons__QxOme > .Button_button__bShZq').click()
+        cy.get('.InputMask_input__wMoYl').type(a)
+        cy.get('.Button_greenButton__UlmDG').click()
+        cy.wait(200)
+        cy.get('.Input_input__gaPT2').type('Hello bugs')
+        cy.get('.Button_greenButton__UlmDG').click()
+        cy.get('.Button_greenButton__UlmDG').type('202020')
+        cy.get('.Button_greenButton__UlmDG').click()
+        cy.wait(200)
     })
 })
 
