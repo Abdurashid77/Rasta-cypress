48b8d3686a0716c37bcd2108f6d5cbf9b4dc3991
Abdurashid77 <abdurj77777@gmail.com>
2022-12-05 09:24:36 +0500

    Order with card, order history

---
+++ cypress/e2e/Rasta/order_history.cy.js
@@ -0,0 +1,47 @@
+describe('example to-do app', () => {
+    beforeEach(() => {
+      cy.visit('https://rasta.uz/')
+    })
+    it('Login', () => {
+
+        //Start-login
+
+        cy.wait(200)
+        cy.get('.Header_buttons__QxOme > .Button_button__bShZq').click()
+        cy.wait(200)
+        cy.get('.InputMask_input__wMoYl').type('996031915')
+        cy.wait(200)
+        cy.get('.Button_greenButton__UlmDG').click()
+        cy.wait(200)
+        cy.get(':nth-child(2) > .InputMask_inputBox__kdkmE > .InputMask_input__wMoYl').type('202020')
+        cy.wait(200)
+        cy.get('.Button_greenButton__UlmDG').click()
+        cy.wait(500)
+
+        //End-login
+
+        //Start-search-restaurant-and-create-order
+
+        cy.get('.Input_input__gaPT2').type('Test Dokon Toshkent (kokcha)')
+        cy.wait(500)
+        cy.get('.Header_restaurantItem__8SCd3').click()
+        cy.wait(300)
+        cy.get(':nth-child(3) > .ProductList_body__wrcJI > .Product_product__OCH58 > .Product_info__UIIg6').click({force: true})
+        cy.wait(200)
+        cy.get('.ProductModal_order__gUggN > .Button_greenButton__UlmDG').click({force: true})
+        cy.wait(200)
+        cy.get('.Cart_button__mKzap > .Button_greenButton__UlmDG').click({force: true})
+        cy.wait(200)
+        cy.get('.CheckoutPayment_list__cZ2_D > :nth-child(1)').click({force: true})
+        cy.wait(300)
+        cy.get('.CheckoutPriceInfo_priceInfo__HIlqr > .Button_greenButton__UlmDG').click({force: true})
+        cy.wait(1000)
+        cy.get('.OrderSuccessDialog_dialog__Md6Je > .Button_greenButton__UlmDG').click({force: true})
+        cy.wait(300)
+        cy.get('.Header_profile__OULED').click({force: true})
+        cy.wait(300)
+        cy.get('[href="/user/orders"]').click({force: true})
+
+
+    })
+    })
